a9e73c7030f68ec2c7d4eb118b51c9bf08654a8d
Sebastien Thibert <sebastien.thibert@marks-and-spencer.com>
2018-08-10 10:44:00 +0100

    Initial Commit

---
+++ MovieDatabase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieDatabase
+//
+//  Created by Sebastien Thibert on 10/08/2018.
+//  Copyright © 2018 Sebastien Thibert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
